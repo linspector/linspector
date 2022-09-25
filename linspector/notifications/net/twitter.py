7572cfc49330e3f4b71be99565b058e48100a2b2
Johannes Findeisen <you@hanez.org>
2022-09-25 06:33:23 +0200

    Typo fix.

--- linspector/notifications/net/twitter.py
+++ linspector/notifications/net/twitter.py
@@ -22,7 +22,7 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 from logging import getLogger
-from linspector.core.notification import Notification
+from linspector.notifications.notification import Notification
 
 logger = getLogger('linspector')
 
