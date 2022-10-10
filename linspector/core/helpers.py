4ae153f3beb237c0eb451e30730d3dac41c788f4
Johannes Findeisen <you@hanez.org>
2022-10-11 01:14:24 +0200

    Added process name and thread ID to logging.

--- linspector/core/helpers.py
+++ linspector/core/helpers.py
@@ -12,25 +12,33 @@ def log(level, msg):
     # only use inspect when log level NOTSET or DEBUG  is enabled.
     if logger.isEnabledFor(0) or logger.isEnabledFor(10):
         import inspect
+        import multiprocessing
+        import threading
+        current_process = multiprocessing.current_process()
         from_stack = inspect.stack()[1]
         function_name = from_stack.function
         line_number = str(from_stack.lineno)
         module_name = inspect.getmodule(from_stack[0]).__name__
         if level == 'critical':
-            logger.critical('[' + module_name + ']:[' + function_name + ']:[' + line_number + '] ' +
-                            str(msg))
+            logger.critical('[' + str(current_process.name) + ']:[' + str(threading.get_ident()) +
+                            ']:[' + str(threading.get_native_id()) + '] [' + module_name + ']:[' +
+                            function_name + ']:[' + line_number + '] ' + str(msg))
         if level == 'error':
-            logger.error('[' + module_name + ']:[' + function_name + ']:[' + line_number + '] ' +
-                         str(msg))
+            logger.error('[' + str(current_process.name) + ']:[' + str(threading.get_ident()) +
+                         ']:[' + str(threading.get_native_id()) + '] [' + module_name + ']:[' +
+                         function_name + ']:[' + line_number + '] ' + str(msg))
         elif level == 'warning':
-            logger.warning('[' + module_name + ']:[' + function_name + ']:[' + line_number + '] ' +
-                           str(msg))
+            logger.warning('[' + str(current_process.name) + ']:[' + str(threading.get_ident()) +
+                           ']:[' + str(threading.get_native_id()) + '] [' + module_name + ']:[' +
+                           function_name + ']:[' + line_number + '] ' + str(msg))
         elif level == 'info':
-            logger.info('[' + module_name + ']:[' + function_name + ']:[' + line_number + '] ' +
-                        str(msg))
+            logger.info('[' + str(current_process.name) + ']:[' + str(threading.get_ident()) +
+                        ']:[' + str(threading.get_native_id()) + '] [' + module_name + ']:[' +
+                        function_name + ']:[' + line_number + '] ' + str(msg))
         elif level == 'debug':
-            logger.debug('[' + module_name + ']:[' + function_name + ']:[' + line_number + '] ' +
-                         str(msg))
+            logger.debug('[' + str(current_process.name) + ']:[' + str(threading.get_ident()) +
+                         ']:[' + str(threading.get_native_id()) + '] [' + module_name + ']:[' +
+                         function_name + ']:[' + line_number + '] ' + str(msg))
     else:
         if level == 'critical':
             logger.critical(str(msg))
