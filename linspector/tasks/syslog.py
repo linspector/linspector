4da46438cc96c2f0a6dc2b6055bd504c7242655b
Johannes Findeisen <you@hanez.org>
2023-01-23 19:16:46 +0100

    Added Syslog Task and renamed FileLogger Task to File.

--- linspector/tasks/syslog.py
+++ linspector/tasks/syslog.py
@@ -1,18 +1,17 @@
 """
 This file is part of Linspector (https://linspector.org/)
-Copyright (c) 2022 Johannes Findeisen <you@hanez.org>. All Rights Reserved.
+Copyright (c) 2023 Johannes Findeisen <you@hanez.org>. All Rights Reserved.
 See LICENSE (MIT license)
 """
 from linspector.core.task import Task
 
 
 def create(configuration, environment, log):
-    return FileLoggerTask(configuration, environment, log)
+    return SyslogTask(configuration, environment, log)
 
 
 # TODO: check for all required configuration options and set defaults if needed.
-class FileLoggerTask(Task):
-
+class SyslogTask(Task):
     def __init__(self, configuration, environment, log):
         super().__init__(configuration, environment, log)
         self.__configuration = configuration
