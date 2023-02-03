d7192913e0ba5cb9f36b750fe9af0b7f4b0a2932
Johannes Findeisen <you@hanez.org>
2023-02-03 04:17:40 +0100

    Tons of fixes, improvements and ideas...

--- linspector/core/singleton.py
+++ linspector/core/singleton.py
@@ -1,7 +1,7 @@
 """
 This file is part of Linspector (https://linspector.org/)
 Copyright (c) 2022 Johannes Findeisen <you@hanez.org>. All Rights Reserved.
-See LICENSE (MIT license)
+See LICENSE.txt (MIT license).
 """
 
 #see http://stackoverflow.com/questions/42558/python-and-the-singleton-pattern
--- linspector/notifications/sms.py
+++ linspector/notifications/sms.py
@@ -1,7 +1,7 @@
 """
 This file is part of Linspector (https://linspector.org/)
 Copyright (c) 2022 Johannes Findeisen <you@hanez.org>. All Rights Reserved.
-See LICENSE (MIT license)
+See LICENSE.txt (MIT license).
 """
 import gammu
 
