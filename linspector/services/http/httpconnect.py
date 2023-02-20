8cc68910e55de860883bbfa5004189978b7faf05
Johannes Findeisen <you@hanez.org>
2023-02-20 09:27:47 +0100

    Added README files to etc/ and etc.dev/ .

--- linspector/services/http/httpconnect.py
+++ linspector/services/http/httpconnect.py
@@ -7,11 +7,13 @@ from linspector.core.service import Service
 
 
 def create(configuration, environment, log):
-    return HTTPKeywordService(configuration, environment, log)
+    return HTTPConnectService(configuration, environment, log)
 
 
-# Get a true or false when a keyword is found in a http request.
-class HTTPKeywordService(Service):
+# Get the status code returned by a http request. Action handling is configured using kwargs. So,
+# let's say all below 4** are no errors and others are an error. Or maybe define ranges, or just
+# maybe the status code that produces an error.
+class HTTPConnectService(Service):
     def __init__(self, configuration, environment, log):
         super().__init__(configuration, environment, log)
         self.__configuration = configuration
