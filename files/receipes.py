ac99346ac9cf7b74488e90bb1f85452953aeb2ef
Johannes Findeisen <you@hanez.org>
2023-02-23 23:12:48 +0100

    Adde files/receipes.py for code snippet storage. (0.21)

---
+++ files/receipes.py
@@ -0,0 +1,4 @@
+# get system timezone name
+# https://stackoverflow.com/questions/1111056/get-time-zone-information-of-the-system-in-python
+import datetime
+tz_string = datetime.datetime.now(datetime.timezone.utc).astimezone().tzname()
