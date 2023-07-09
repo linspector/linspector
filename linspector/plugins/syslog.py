afc2f23308cec9b07c620b0678b6f2558c52a595
Johannes Findeisen <you@hanez.org>
2023-07-09 23:57:54 +0200

    Added syslog plugin stub but just as an idea. (0.21.6)

---
+++ linspector/plugins/syslog.py
@@ -0,0 +1,25 @@
+"""
+This file is part of Linspector (https://linspector.org/)
+Copyright (c) 2023 Johannes Findeisen <you@hanez.org>. All Rights Reserved.
+See LICENSE (MIT license).
+"""
+# TODO: Think about this idea it little bit more. Maybe Syslog support should not become a plugin
+#  but should better be implemented in the core.
+from linspector.plugin import Plugin
+
+
+def create(configuration, environment, linspector, log):
+    return SyslogPlugin(configuration, environment, linspector, log)
+
+
+# TODO: check for all required configuration options and set defaults if needed.
+class SyslogPlugin(Plugin):
+    def __init__(self, configuration, environment, linspector, log):
+        super().__init__(configuration, environment, linspector, log)
+        self.__configuration = configuration
+        self.__environment = environment
+        self.__linspector = linspector
+        self.__log = log
+
+    def run(self):
+        return
