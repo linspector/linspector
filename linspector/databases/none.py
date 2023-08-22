b50946b097057c751e5f8f74dc2d6d208f8b7bf1
Johannes Findeisen <you@hanez.org>
2023-08-22 09:43:41 +0200

    Renamed dummy database module to "none". (0.25.3)

--- linspector/databases/none.py
+++ linspector/databases/none.py
@@ -8,14 +8,15 @@ from linspector.database import Database
 
 
 def create(configuration, environment, log):
-    return DummyDBDatabase(configuration, environment, log)
+    return NoneDatabase(configuration, environment, log)
 
 
-class DummyDBDatabase(Database):
+class NoneDatabase(Database):
     """
     This is a dummy database module to make sure all monitors can work without errors even when no
     database is configured. It should always be loaded or only if no other database is selected.
     """
 
-    def insert(self, host, identifier, json, message, service, status, timestamp):
+    @staticmethod
+    def insert(host, identifier, json, message, service, status, timestamp):
         pass
